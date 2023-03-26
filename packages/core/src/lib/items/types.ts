ec21b9dbe29c6280e703a12ea83091f3c178330c
Benjamin Zamour <benjamin.zamour@gmail.com>
2023-03-26 17:31:37 -0400

    core/items/types: Update items types

--- packages/core/src/lib/items/types.ts
+++ packages/core/src/lib/items/types.ts
@@ -1,9 +1,48 @@
-import { DamageType } from '../health/type';
+export const CurrencyShortNames = ['pp', 'ep', 'gp', 'sp', 'cp'] as const;
+type CurrencyShortNamesTuple = typeof CurrencyShortNames;
+export type CurrencyShortName = CurrencyShortNamesTuple[number];
+
+export type MarketValue = {
+  [k in CurrencyShortName]?: number;
+};
+
+export const ItemTypes = [
+  'adventuring-gear',
+  'ammunition',
+  'arcane-foci',
+  'armor',
+  'artisan-tools',
+  'druidic-foci',
+  'equipment-pack',
+  'gaming-set',
+  'armor',
+  'holy-symbol',
+  'kit',
+  'weapon',
+  'animal',
+  'vehicle',
+  'musical-instrument',
+  'other-tool',
+  'potion',
+  'ring',
+  'rod',
+  'scroll',
+  'shield',
+  'staff',
+  'standard-gear',
+  'tool',
+  'wand',
+  'wondrous-item',
+] as const;
+
+type ItemTypesTuple = typeof ItemTypes;
+export type ItemType = ItemTypesTuple[number];
 
 export interface Item<T = unknown> {
   name: string;
-  cost: number;
+  itemType: ItemType;
+  cost: MarketValue;
   weight: number;
-  description?: string;
+  description: string;
   extra?: T;
 }
